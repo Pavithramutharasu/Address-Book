3c37b2b2215ee4509da2148913b38ae6370fd41c
Pavithra Mutharasu <pavithra.dof@gmail.com>
2025-05-20 10:53:50 +0530

    Create RestController and Set Application Properties

---
+++ src/main/java/com/bridgelaz/addressbook/controller/AddressController.java
@@ -0,0 +1,43 @@
+package com.bridgelaz.addressbook.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelaz.addressbook.model.AddressModel;
+
+@RestController
+@RequestMapping("/addressbook")
+public class AddressController {
+	@GetMapping("/")
+    public ResponseEntity<String> getAllContacts() {
+        return ResponseEntity.ok("Getting all address book contacts");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getContactById(@PathVariable int id) {
+        return ResponseEntity.ok("Getting contact with ID: " + id);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createContact(@RequestBody AddressModel contact) {
+        return ResponseEntity.ok("Created contact: " + contact.getName());
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateContact(@PathVariable int id, @RequestBody AddressModel contact) {
+        return ResponseEntity.ok("Updated contact with ID: " + id + ", Name: " + contact.getName());
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteContact(@PathVariable int id) {
+        return ResponseEntity.ok("Deleted contact with ID: " + id);
+    }
+
+}
---
+++ src/main/java/com/bridgelaz/addressbook/model/AddressModel.java
@@ -0,0 +1,26 @@
+package com.bridgelaz.addressbook.model;
+
+public class AddressModel {
+	private int id;
+    private String name;
+    private String email;
+
+    public AddressModel() {}
+
+    public AddressModel(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+}
