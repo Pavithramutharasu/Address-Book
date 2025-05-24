2c54c9c57cd14e2cb85ac7729028457088656e1e
Pavithra Mutharasu <pavithra.dof@gmail.com>
2025-05-24 10:47:03 +0530

    Adding Lombok Library

--- src/main/java/com/bridgelaz/addressbook/dto/AddressDTO.java
+++ src/main/java/com/bridgelaz/addressbook/dto/AddressDTO.java
@@ -1,21 +1,17 @@
 package com.bridgelaz.addressbook.dto;
 
+import com.bridgelaz.addressbook.model.AddressModel;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddressDTO {
 	private String name;
     private String email;
 
-    public AddressDTO() {}
-
-    public AddressDTO(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    // Getters and Setters
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
+   
 }
